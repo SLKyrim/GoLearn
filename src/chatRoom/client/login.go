add08b93f758e5a8246f3eb2e2d0af7f201b4ab1
Single Long <623962644@qq.com>
2020-05-16 17:00:16 +0800

    通讯系统：完成界面

---
+++ src/chatRoom/client/login.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// 写一个函数，完成登陆
+func login(userId int, userPwd string) (err error) {
+
+	// 指定协议
+	fmt.Printf("userId = %d userPwd = %s\n", userId, userPwd)
+
+	return nil
+
+}
