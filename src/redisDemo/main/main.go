5b3e3c834c46aba6022fd66141d96e9bc7fcaae1
Single Long <623962644@qq.com>
2020-05-16 15:35:48 +0800

    Go向Redis读写数据

--- src/redisDemo/main/main.go
+++ src/redisDemo/main/main.go
@@ -14,5 +14,22 @@ func main() {
 		fmt.Println("redis.Dial err=", err)
 		return
 	}
-	fmt.Println("conn succ...", conn)
+	defer conn.Close() // 重要：延时关闭
+
+	// 2. 通过go向redis写入数据 string [key-val]
+	_, err = conn.Do("Set", "name", "SingleLong")
+	if err != nil {
+		fmt.Println("set err=", err)
+		return
+	}
+
+	// 3. 通过go向redis读取数据 string [key-val]
+	// r 是 interface{}，需要类型转换
+	r, err := redis.String(conn.Do("Get", "name"))
+	if err != nil {
+		fmt.Println("get err=", err)
+		return
+	}
+
+	fmt.Println("操作成功", r)
 }
